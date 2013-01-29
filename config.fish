b496d856c58d41ee50e91fdc48d10e1eed18af04
Vic Goldfeld <vic@longstorm.org>
2013-01-29 09:21:28 -0200

    wrap git to use my personal credentials for vim bundles

--- config.fish
+++ config.fish
@@ -2,6 +2,8 @@ function git
 	switch $PWD
 		case $HOME/goldfeld/*
 			command git -c user.email=vic@longstorm.org -c user.name="Vic Goldfeld" $argv
+		case $HOME/.vim/*/*
+			command git -c user.email=vic@longstorm.org -c user.name="Vic Goldfeld" $argv
 		case '**'
 			command git $argv
 	end
