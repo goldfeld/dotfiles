744611fda85fb553a04a5fc06b9c5aeb960cf87b
Vic Goldfeld <vic@longstorm.org>
2013-11-14 03:07:18 -0200

    add basic link insertion bind to mimic my vim one

--- emacs/org-mode.el
+++ emacs/org-mode.el
@@ -1,6 +1,26 @@
 (add-hook 'org-load-hook
 	   (lambda ()
+	     (define-key org-mode-map "\C-t\C-lb" 'org-open-at-point)
+	     (define-key org-mode-map "\C-t\C-lw" 'my-org-insert-link)
 	     (define-key org-mode-map "\C-n" 'org-next-link)
 	     (define-key org-mode-map "\C-p" 'org-previous-link)))
 
-(define-key org-mode-map "\C-t\C-lb" 'org-open-at-point)
+(defun my-org-insert-link ()
+  "Insert org link where default description is set to html title."
+  (interactive)
+  (let* ((url (thing-at-point 'line))
+         (title (get-html-title-from-url url)))
+    (org-insert-link nil url title)))
+
+(require 'mm-url) ; to include mm-url-decode-entities-string
+
+(defun get-html-title-from-url (url)
+  "Return content in <title> tag."
+  (let (x1 x2 (download-buffer (url-retrieve-synchronously url)))
+    (save-excursion
+      (set-buffer download-buffer)
+      (beginning-of-buffer)
+      (setq x1 (search-forward "<title>"))
+      (search-forward "</title>")
+      (setq x2 (search-backward "<"))
+      (mm-url-decode-entities-string (buffer-substring-no-properties x1 x2)))))
