47610f3c410018af1df595b1b82c75524102f0d8
Vic Goldfeld <vic@longstorm.org>
2014-01-13 13:32:49 -0200

    add dmenu completion stub to emacs file

--- emacs/projectile.el
+++ emacs/projectile.el
@@ -1,6 +1,24 @@
 (projectile-global-mode)
 (setq projectile-use-git-grep t)
 
+(defun dmenu-complete (prompt choices)
+  (interactive)
+  (shell-command "cd ~/hol && git checkout src/main/webapp/public/assets")
+  (let* ((stripped-choices
+          (-uniq (--map (file-name-nondirectory it) choices)))
+         (choice
+          (ido-completing-read prompt stripped-choices))
+         (matching-files
+          (-filter
+           (lambda (file)
+             (equal (file-name-nondirectory file) choice))
+           choices)))
+    (if (> (length matching-files) 1)
+        (ido-completing-read prompt matching-files)
+      (car matching-files))))
+
+;(setq projectile-completion-system 'dmenu-complete)
+
 ;(let ((projectile-file (expand-file-name "~/.emacs.d/projectile-bookmarks.eld")))
 ;  (delete-file projectile-file)
 ;  (append-to-file "(" nil projectile-file)
