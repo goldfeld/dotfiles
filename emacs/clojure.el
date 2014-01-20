371fc5d0335298d09c0ad3505ed47a189af2b59f
Vic Goldfeld <vic@longstorm.org>
2014-01-20 08:33:22 +0000

    update nrepl configs to new cider name

--- emacs/clojure.el
+++ emacs/clojure.el
@@ -1,7 +1,7 @@
-(setq nrepl-hide-special-buffers t)
-(setq nrepl-popup-stacktraces nil)
-(setq nrepl-popup-stacktraces-in-repl t)
-(add-to-list 'same-window-buffer-names "*nrepl*")
+(setq cider-hide-special-buffers t)
+(setq cider-popup-stacktraces nil)
+(setq cider-popup-stacktraces-in-repl t)
+(add-to-list 'same-window-buffer-names "*cider*")
 
 (define-global-abbrev "defnn" "defn ^:private")
 
@@ -15,32 +15,32 @@
 
 (require 'ac-nrepl)
 
-(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+(add-hook 'nrepl-interaction-mode-hook 'cider-turn-on-eldoc-mode)
 (add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
 
-(add-hook 'nrepl-mode-hook 'subword-mode)
-;(add-hook 'nrepl-mode-hook 'paredit-mode)
-;(add-hook 'nrepl-mode-hook 'smartparens-mode)
-(add-hook 'nrepl-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+(add-hook 'cider-mode-hook 'subword-mode)
+;(add-hook 'cider-mode-hook 'paredit-mode)
+;(add-hook 'cider-mode-hook 'smartparens-mode)
+(add-hook 'cider-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'cider-mode-hook 'ac-nrepl-setup)
 
-(eval-after-load "auto-complete" '(add-to-list 'ac-modes 'nrepl-mode))
+(eval-after-load "auto-complete" '(add-to-list 'ac-modes 'cider-mode))
 
-(define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
+;(define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
 
 (defun set-auto-complete-as-completion-at-point-function ()
   (setq completion-at-point-functions '(auto-complete)))
 (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
-(add-hook 'nrepl-mode-hook 'set-auto-complete-as-completion-at-point-function)
+(add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
 (add-hook 'nrepl-interaction-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
-;(define-key nrepl-mode-map "\C-t\C-s\C-l" 'nrepl-clear-buffer)
-(add-hook 'nrepl-mode-hook
+;(define-key cider-mode-map "\C-t\C-s\C-l" 'cider-clear-buffer)
+(add-hook 'cider-mode-hook
       '(lambda ()
 	 (paredit-mode t)
 	 ;(load-theme-buffer-local 'cyberpunk (current-buffer))
-	 (define-key evil-insert-state-local-map (kbd "<up>") 'nrepl-backward-input)
-	 (define-key evil-insert-state-local-map (kbd "<down>") 'nrepl-forward-input)
-	 (define-key evil-insert-state-local-map (kbd "C-l") 'nrepl-clear-buffer)
-	 (define-key evil-normal-state-local-map ",," 'nrepl-switch-to-last-clojure-buffer)))
+	 (define-key evil-insert-state-local-map (kbd "<up>") 'cider-backward-input)
+	 (define-key evil-insert-state-local-map (kbd "<down>") 'cider-forward-input)
+	 (define-key evil-insert-state-local-map (kbd "C-l") 'cider-clear-buffer)
+	 (define-key evil-normal-state-local-map ",," 'cider-switch-to-last-clojure-buffer)))
