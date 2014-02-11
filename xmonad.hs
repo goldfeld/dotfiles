5b86fc386509ead54b603628edff1d44aaa103e1
Vic Goldfeld <vic@longstorm.org>
2014-02-11 16:25:44 -0200

    add alternative to xmonad Enter-based keybinds

--- xmonad.hs
+++ xmonad.hs
@@ -2,12 +2,17 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ServerMode
+import XMonad.Util.Run (spawnPipe)
 import XMonad.Actions.Commands
-import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Actions.UpdatePointer
+import qualified XMonad.StackSet as W
+import XMonad.Util.EZConfig (additionalKeysP)
 import System.IO
 
-main = xmonad defaultConfig
+main = xmonad $ defaultConfig
      { handleEventHook = serverModeEventHook
+     , focusFollowsMouse = False
      , terminal = "terminator"
-     }
+     } `additionalKeysP` [ ("M-S-u", spawn "terminator")
+                         , ("M-u", windows W.swapMaster)
+                         ]
