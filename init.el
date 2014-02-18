fc57cb4e09d639790500d2a446460efe0714c771
Vic Goldfeld <vic@longstorm.org>
2014-02-18 15:46:20 -0300

    disable emacs lockfile creation (e.g. #file.js)

--- init.el
+++ init.el
@@ -10,6 +10,7 @@
 
 (setq backup-directory-alist `((".*" . ,temporary-file-directory)))
 (setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
+(setq create-lockfiles nil)
 
 (setq browse-url-browser-function 'browse-url-generic
       browse-url-generic-program "luakit")
