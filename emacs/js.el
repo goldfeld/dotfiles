aa58a1d4dc306ed852aedf189885879259df9bc8
Vic Goldfeld <vic@longstorm.org>
2014-05-09 07:43:42 -0300

    set js2-mode indentatioo to 2 spaces

--- emacs/js.el
+++ emacs/js.el
@@ -1,5 +1,6 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+(setq js2-basic-offset 2)
 
 (defun grunt-find-root ()
   (let ((root (substring (shell-command-to-string "git rev-parse --show-toplevel") 0 -1)))
