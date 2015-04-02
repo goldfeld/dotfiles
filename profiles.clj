5e65e0f05537d6ff1ccc4423ff05123faa83c660
Vic Goldfeld <vic@longstorm.org>
2015-04-02 09:40:28 -0300

    rm from profiles.clj the deps overriding reptile's

--- profiles.clj
+++ profiles.clj
@@ -1,5 +1,3 @@
-{:user {:plugins [[lein-difftest "2.0.0"]
-                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]]
-        :dependencies [[leiningen "2.5.1"]
-                       [org.clojure/tools.namespace "0.2.4"]
+{:user {:plugins [[lein-difftest "2.0.0"]]
+        :dependencies [[org.clojure/tools.namespace "0.2.4"]
                        [org.clojure/tools.trace "0.7.6"]]}}
